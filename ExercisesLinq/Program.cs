09ad7a460be58e66207ff4121d5e477c323fe84a
CGDM101 <51097129+CGDM101@users.noreply.github.com>
2021-11-15 15:07:38 +0100

    Add project files.

---
+++ ExercisesLinq/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ExercisesLinq
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
