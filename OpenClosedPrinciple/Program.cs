304f3c0c92d97324881d15a194b5d93689bf3feb
CGDM101 <51097129+CGDM101@users.noreply.github.com>
2021-12-28 15:28:50 +0100

    Single Responsibility Principle - First commit

---
+++ OpenClosedPrinciple/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace OpenClosedPrinciple
+{
+    public class Rectangle
+    {
+        public double Width { get; set; }
+        public double Height { get; set; }
+    }
+    public class AreaCalculator 
+    {
+        public double Area(Rectangle[] shapes)
+        {
+            double area = 0;
+            foreach (var shape in shapes)
+            {
+                area += shape.Width * shape.Height;
+            }
+
+            return area;
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+        }
+    }
+}
