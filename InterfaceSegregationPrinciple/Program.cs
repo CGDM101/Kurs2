672c6707294ccdeffd88da5770f25b5cf21fe8a6
CGDM101 <51097129+CGDM101@users.noreply.github.com>
2021-12-28 16:58:06 +0100

    Start InterfaceSegregationPrinciple.

---
+++ InterfaceSegregationPrinciple/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace InterfaceSegregationPrinciple
+{
+    public interface IAttackeable
+    {
+        int BluntAttack();
+        int ShootArrow();
+        int CastSpell();
+    }
+
+    public class Warrior : IAttackeable
+    {
+        public int BluntAttack() => throw new NotImplementedException();
+        public int CastSpell() => throw new NotImplementedException();
+        public int ShootArrow() => throw new NotImplementedException();
+
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
