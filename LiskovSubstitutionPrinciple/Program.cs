f51cbeb2a021a1c9d3f6291bbbf1a588fcafccba
CGDM101 <51097129+CGDM101@users.noreply.github.com>
2021-12-28 16:22:53 +0100

    Added dog.

--- LiskovSubstitutionPrinciple/Program.cs
+++ LiskovSubstitutionPrinciple/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace LiskovSubstitutionPrinciple
 {
@@ -13,9 +14,25 @@ namespace LiskovSubstitutionPrinciple
     {
         public void Play()
         {
-            Cat c = new Cat() { Name = "Misse" };
-            Console.WriteLine(c.MakeSound());
+            var pets = new List<Cat>() 
+            {
+                new Cat { Name = "Misse" },
+                new Dog { Name = "Voffsing" },
+            };
+            foreach (var pet in pets)
+            {
+                Console.WriteLine(pet.MakeSound());
+            }
+
+
+            //Cat c = new Cat() { Name = "Misse" };
+            //Console.WriteLine(c.MakeSound());
         }
+    }
+
+    public class Dog : Cat
+    {
+        private string Sound = "Wooff";
     }
     class Program
     {
