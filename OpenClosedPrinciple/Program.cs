21726078b063d7d180b43918d12f89cabcacecdf
CGDM101 <51097129+CGDM101@users.noreply.github.com>
2021-12-28 15:35:30 +0100

    Förändring utan OpenClosedPrinciple.

--- OpenClosedPrinciple/Program.cs
+++ OpenClosedPrinciple/Program.cs
@@ -9,16 +9,31 @@ namespace OpenClosedPrinciple
     }
     public class AreaCalculator 
     {
-        public double Area(Rectangle[] shapes)
+        public double Area(object[] shapes) // Rectangle[] shapes förut
         {
             double area = 0;
             foreach (var shape in shapes)
-            {
+                if (shape is Rectangle)
+                {
+                    Rectangle rectangle = (Rectangle)shape;
+                    area += rectangle.Width * rectangle.Height;
+                }
+                else
+                {
+                    Circle circle = (Circle)shape;
+                    area += circle.Radius * circle.Radius * Math.PI;
+                }
+            /*{
                 area += shape.Width * shape.Height;
-            }
+            }*/
 
             return area;
         }
+    }
+
+    public class Circle // Om vi vill ha cirkel måste vi ändra i AreaCalculator ovan... Blir fult.
+    {
+        public double Radius { get; set; }
     }
 
     class Program
