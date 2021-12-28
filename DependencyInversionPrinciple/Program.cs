8a4e25e738f694e58ba2f8e50328fcff0d6085c3
CGDM101 <51097129+CGDM101@users.noreply.github.com>
2021-12-28 17:26:15 +0100

    Update.

--- DependencyInversionPrinciple/Program.cs
+++ DependencyInversionPrinciple/Program.cs
@@ -3,6 +3,35 @@ using System.Collections.Generic;
 
 namespace DependencyInversionPrinciple
 {
+    public class Game
+    {
+        List<IPlayeableCharacter> Players = new List<IPlayeableCharacter>();
+        public void StartGame()
+        {
+            Players = new List<IPlayeableCharacter>
+            {
+                new Warrior(),
+                new Archer(),
+                new Mage(),
+                new Elf(),
+            };
+        }
+
+        //private Warrior conan = new Warrior();
+        //private Archer legolas = new Archer();
+        //private Mage merlin = new Mage();
+        //private Elf willFerrell = new Elf();
+    }
+
+    public interface IPlayeableCharacter
+    {
+        public string Name { get; set; }
+        public int XP { get; set; }
+        public int HP { get; set; }
+        public int XPNeeded { get; set; }
+        public int Level { get; set; }
+
+    }
     public interface IAttackeable
     {
         int BluntAttack();
@@ -24,19 +53,19 @@ namespace DependencyInversionPrinciple
         int CastSpell();
     }
 
-    public class Warrior : IBluntable
+    public class Warrior : IBluntable, IPlayeableCharacter
     {
         public int BluntAttack() => throw new NotImplementedException();
     }
-    public class Archer : IArcheryable
+    public class Archer : IArcheryable, IPlayeableCharacter
     {
         public int ShootArrow() => throw new NotImplementedException();
     }
-    public class Mage : IMagicable
+    public class Mage : IMagicable, IPlayeableCharacter
     {
         public int CastSpell() => throw new NotImplementedException();
     }
-    public class Elf : IBluntable, IArcheryable, IMagicable
+    public class Elf : IBluntable, IArcheryable, IMagicable, IPlayeableCharacter
     {
         public int BluntAttack() => throw new NotImplementedException();
         public int ShootArrow() => throw new NotImplementedException();
