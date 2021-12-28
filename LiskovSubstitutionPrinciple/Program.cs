53f408ac1597a7e4f18831df3512e2c48e9ef023
CGDM101 <51097129+CGDM101@users.noreply.github.com>
2021-12-28 16:11:16 +0100

    First commit LiskovSubstitutionPrinciple.

---
+++ LiskovSubstitutionPrinciple/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace LiskovSubstitutionPrinciple
+{
+    public class Cat 
+    {
+        private string Sound { get; set; } = "Meeeeoooowwwww";
+        public string Name { get; set; }
+        public string MakeSound() => $"{Name} is a nice {GetType().Name} who says {Sound}";
+    }
+
+    public class PlayWithPets
+    {
+        public void Play()
+        {
+            Cat c = new Cat() { Name = "Misse" };
+            Console.WriteLine(c.MakeSound());
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
